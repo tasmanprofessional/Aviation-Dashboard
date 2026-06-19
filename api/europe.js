89cbe8b3896ea1c45fd00929003bec469ce12dfb
tasmanprofessional <tasmanprofessional@gmail.com>
2026-06-20 09:39:17 +1000

    Update europe.js

--- api/europe.js
+++ api/europe.js
@@ -1,11 +1,22 @@
 export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://api.adsb.lol/v2/lat/51.47/lon/-0.45/dist/250"
+    );
 
-const response = await fetch(
-"https://api.adsb.lol/v2/lat/51.47/lon/-0.45/dist/250"
-);
+    const text = await response.text();
 
-const text = await response.text();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Cache-Control", "s-maxage=30");
 
-res.status(200).send(text);
+    res.status(200).send(text);
 
+  } catch (err) {
+
+    res.status(500).json({
+      success: false,
+      error: String(err)
+    });
+
+  }
 }
