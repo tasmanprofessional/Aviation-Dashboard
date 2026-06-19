c8754b9149c72047059dabb6af7d9539adf5ee8b
tasmanprofessional <tasmanprofessional@gmail.com>
2026-06-20 08:07:02 +1000

    Update proxy.js

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,14 +1,6 @@
 export async function onRequest(context) {
   try {
-    const credentials = "tasmanprofessional:Lemons11";
-    const encoded = btoa(unescape(encodeURIComponent(credentials)));
-    
-    const response = await fetch("https://opensky-network.org/api/states/all", {
-      headers: {
-        "Authorization": "Basic " + encoded
-      }
-    });
-    
+    const response = await fetch("https://api.adsb.lol/v2/ladd");
     const text = await response.text();
 
     return new Response(text, {
