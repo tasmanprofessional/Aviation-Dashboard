8bfbd731577a54de32e34e952eb8ef58c18c9b1f
tasmanprofessional <tasmanprofessional@gmail.com>
2026-06-20 07:43:25 +1000

    Update proxy.js

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,14 +1,40 @@
 export async function onRequest(context) {
-  const response = await fetch("https://opensky-network.org/api/states/all", {
-    headers: {
-      "Authorization": "Basic " + btoa("tasmanprofessional:Lemons11")
+  try {
+    const response = await fetch("https://opensky-network.org/api/states/all", {
+      headers: {
+        "Authorization": "Basic " + btoa("tasmanprofessional:Lemons11")
+      }
+    });
+    
+    const text = await response.text();
+    
+    if (!response.ok) {
+      return new Response(JSON.stringify({ 
+        error: response.status, 
+        message: text 
+      }), {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        }
+      });
     }
-  });
-  const data = await response.json();
-  return new Response(JSON.stringify(data), {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
-    }
-  });
+
+    return new Response(text, {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      }
+    });
+
+  } catch (err) {
+    return new Response(JSON.stringify({ error: err.message }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      }
+    });
+  }
 }
