1b9d877a7b2cfba73b66684d4e848e7faa85c137
tasmanprofessional <tasmanprofessional@gmail.com>
2026-06-19 12:28:56 +1000

    Update opensky.js

--- netlify/functions/opensky.js
+++ netlify/functions/opensky.js
@@ -1,36 +1,36 @@
 exports.handler = async function () {
 
-  const username = process.env.OPENSKY_USERNAME;
-  const password = process.env.OPENSKY_PASSWORD;
+  const username = "tasmanprofessional";
+  const password = "Lemons11";
 
   try {
-
-    const auth = Buffer
-      .from(username + ":" + password)
-      .toString("base64");
 
     const response = await fetch(
       "https://opensky-network.org/api/states/all",
       {
         headers: {
-          "Authorization": "Basic " + auth
+          "Authorization":
+            "Basic " + Buffer
+              .from(username + ":" + password)
+              .toString("base64")
         }
       }
     );
 
-    if (!response.ok) {
 
+    if (!response.ok) {
       return {
         statusCode: response.status,
         body: JSON.stringify({
-          error: "OpenSky error",
+          error: "OpenSky returned error",
           status: response.status
         })
       };
-
     }
 
+
     const data = await response.json();
+
 
     return {
       statusCode: 200,
