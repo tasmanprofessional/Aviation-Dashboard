7eceee98159cfe12612d856a5c3acccd0954f30f
tasmanprofessional <tasmanprofessional@gmail.com>
2026-06-20 09:15:31 +1000

    Update europe.js

--- api/europe.js
+++ api/europe.js
@@ -1,12 +1,19 @@
 export default async function handler(req, res) {
   try {
+
     const response = await fetch(
       "https://api.adsb.lol/v2/ladd"
     );
 
     const data = await response.json();
 
-    res.status(200).json(data);
+    const sample =
+      (data.ac || [])[0];
+
+    res.status(200).json({
+      aircraftCount: (data.ac || []).length,
+      sample
+    });
 
   } catch (err) {
 
